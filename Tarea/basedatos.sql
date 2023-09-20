23eb6e5cd3816151b1acb4ffde804c1d9fcc41e7
IsraeelP7 <israelprieto18@aragon.unam.mx>
2023-09-19 22:56:36 -0600

    tareadel19Septiembre

--- Tarea/basedatos.sql
+++ Tarea/basedatos.sql
@@ -1,33 +1,33 @@
--- donde exista una db con el mismo nombre la elimina
-drop schema if exists `fes_aragon`;
 
--- se crear la DB 
-create schema  if not exists `fes_aragon` default  character set utf8 collate  utf8_spanish2_ci;
-USE `fes_aragon`; 
+drop schema if exists `israel_fes_aragon`;
 
--- se crear la tabla
+
+create schema  if not exists `israel_fes_aragon` default  character set utf8 collate  utf8_spanish2_ci;
+USE `israel_fes_aragon`; 
+
+
 CREATE TABLE  `PERSONA`(
 `nombre_usuario` text not null,
 `carrera` text not null,
 `no_cuenta` int(10) not null,
 `direccion` text not null,
-`telefono` varchar (8) not null,
+`telefono` varchar (10) not null,
 `email` text not null,
-`password` varchar (8) not null,
+`password` varchar (10) not null,
 `fecha_registro` datetime not null default current_timestamp,
 `permisos` int (11) not null default '2'
 )engine=Innodb default charset=utf8;
--- define el motor de la db que se va a oucupar
--- agregar dos registros
-insert into `PERSONA`(`nombre_usuario`, `carrera`,`no_cuenta`,`direccion`,`telefono`,`email`,`password`,`fecha_registro`,`permisos`)values
-('Aaron Velasco','ico','413112576','gloria 15','5612315','aaron@gmail.com','123456','2020-05-12 17:40:00',2),
-('Enrique Mariaca','ico','319159572','calle 15','5623387','kike@gmail.com','123456','2023-05-12 19:35:00',2);
 
--- define la llave primaria
+insert into `PERSONA`(`nombre_usuario`, `carrera`,`no_cuenta`,`direccion`,`telefono`,`email`,`password`,`fecha_registro`,`permisos`)values
+('Aaron Velasco','ico','413112576','gloria 15','561267500','elaaron@gmail.com','123456','2020-05-12 17:40:00',2),
+('Israel Prieto','ico','319023718','calle 45','5627519983','israel18223@gmail.com','5627519983','2022-05-12 21:45:00',2);
+
+
 alter table `persona`
   add primary key (`no_cuenta`);
 commit;
 
--- commit confirma la trasaccion o proceso actual, haciendo que sus cambios sean permanentes
+
 
 SELECT * FROM PERSONA;
+
